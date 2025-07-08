4f9b04fab22e87c5250787c9410abbc23bcd65ab
Yruam Käffer de Faria <yruamkf@gmail.com>
2025-07-08 19:57:35 -0300

    Servidor

--- server/src/db/schema/rooms.ts
+++ server/src/db/schema/rooms.ts
@@ -4,5 +4,5 @@ export const rooms = pgTable('rooms', {
     id: uuid().primaryKey().defaultRandom(),
     name: text().notNull(),
     description: text(),
-    createAt: timestamp().defaultNow().notNull(),
+    createdAt: timestamp().defaultNow().notNull(),
 })
--- server/src/server.ts
+++ server/src/server.ts
@@ -2,6 +2,7 @@ import fastifyCors from "@fastify/cors"
 import { fastify } from "fastify"
 import { serializerCompiler, validatorCompiler, type ZodTypeProvider } from "fastify-type-provider-zod"
 import { env } from "./env.ts"
+import { getRoomsRoute } from "./http/routes/get-rooms.ts"
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -15,5 +16,7 @@ app.setValidatorCompiler(validatorCompiler)
 app.get('/health', () => {
     return 'OK'
 })
+
+app.register(getRoomsRoute)
 
 app.listen ({ port: env.PORT })
